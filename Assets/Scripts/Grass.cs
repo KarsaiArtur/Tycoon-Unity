2ae854be5206c8e96955989bab3b5b00d0ed346b
KarsaiArtur <127442850+KarsaiArtur@users.noreply.github.com>
2024-03-05 14:08:31 +0100

    első lépések
    kamera mozgatás, terrain generálás, ui, kocka lerakás

--- Assets/Scripts/Grass.cs
+++ Assets/Scripts/Grass.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Grass : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        int[] rotations = { 0, 90, 180, 270 };
-        transform.Rotate(0, rotations[Random.Range(0, 4)], 0);
+        
     }
-
+    
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Rotate(float angle)
+    {
+        transform.Rotate(0, angle, 0);
     }
 }
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+    public float cameraSpeed = 10;
+    public float zoomSpeed = 10;
+    public Camera GameCamera;
+    private Grass m_Selected = null;
+    public int maxZoom = 5;
+    public int minZoom = 20;
+    public int maxLeft = 50;
+    public int maxRight = 50;
+    public int moveSpeed = 1;
+    public float angle = 45f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Zoom();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleSelection();
+        }
+        if (m_Selected != null)
+        {
+            var ray = GameCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits = Physics.RaycastAll(ray);
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.collider.CompareTag("Terrain"))
+                {
+                    m_Selected.transform.position = new Vector3(Round(hit.point.x), 2, Round(hit.point.z));
+                }
+            }
+        }
+    }
+
+    float Round(float number)
+    {
+        return Mathf.Floor(number) + 0.5f;
+    }
+
+    void Move()
+    {
+        Vector2 move = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+
+        //if (transform.position.x < maxLeft && move.x > 0)
+        //{
+        //    move = new Vector2(0, 0);
+        //}
+        //if (transform.position.z > maxRight && move.y > 0)
+        //{
+        //    move = new Vector2(0, 0);
+        //}                                                                  * 0.5773f
+        transform.position = transform.position + new Vector3(move.y+move.x*(angle/45f), 0, move.x-move.y*(angle/ 45f)) * cameraSpeed * Time.deltaTime;      
+
+        MovementSpeedChange();
+    }
+
+    void MovementSpeedChange()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            cameraSpeed *= 2;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            cameraSpeed /= 2;
+        }
+    }
+
+    void Zoom()
+    {
+        float zoom = Input.GetAxis("Mouse ScrollWheel");
+
+        if (transform.position.y < maxZoom && zoom > 0)
+        {
+            zoom = 0;
+        }
+        if (transform.position.y > minZoom && zoom < 0)
+        {
+            zoom = 0;
+        }
+        transform.Translate(Vector3.forward * zoom * zoomSpeed);
+    }
+
+    public void HandleSelection()
+    {
+        var ray = GameCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var grass = hit.collider.GetComponentInParent<Grass>();
+            m_Selected = grass;
+
+            //m_Selected.transform.position = m_Selected.transform.position + new Vector3(Input.GetAxis("Mouse X"), 0, move.x);
+
+        }
+    }
+
+    public Grass cube;
+
+    public void Spawn()
+    {
+                m_Selected = Instantiate(cube, new Vector3(Round(Input.mousePosition.x), 2, Round(Input.mousePosition.z)), new Quaternion(0,0,0,0));
+    }
+}
